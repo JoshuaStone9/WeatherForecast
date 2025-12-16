40332eec69e31563de02fe4fc309c5159d6d9f0f
JoshuaStone9 <95473823+JoshuaStone9@users.noreply.github.com>
2025-12-16 15:33:19 +0000

    Testing Functionality

--- obj/Debug/net9.0/WeatherForecast.AssemblyInfo.cs
+++ obj/Debug/net9.0/WeatherForecast.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+583600cd2932715c516244b346c8bb9bad021f4e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8eb9afec0fae819897f3f7bb49ad0c77363a2fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherForecast")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
